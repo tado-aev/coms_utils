a62e33b2477b48f0dba47095cc8b5981e8d867fe
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2017-09-16 23:42:21 +0900

    Add script to check dirtiness of repo

---
+++ scripts/check_dirty.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+repos=( "$@" )
+valid_repos=( )
+
+if (( ${#repos[@]} < 1 )); then
+    echo "Not enough arguments" >&2
+    exit 1
+fi
+
+if (( ${#repos[@]} == 1 )); then
+    repos=( "${repos[0]}" ${repos[0]}/* )
+fi
+
+for repo in ${repos[@]}; do
+    if [[ -d ${repo}/.git ]]; then
+        valid_repos=( "${valid_repos[@]}" "$repo" )
+    fi
+done
+
+if (( ${#valid_repos[@]} == 0 )); then
+    echo "No git repo found" >&2
+    exit 1
+fi
+
+for repo in ${valid_repos[@]}; do
+    echo "> $( basename $repo )"
+    res="$( cd $repo && \
+        [[ -z $( git status --short ) ]] && \
+        echo true || echo false)"
+    if $res; then
+        echo -e "\e[32mClean\e[0m"
+    else
+        echo -e "\e[31mDirty\e[0m"
+    fi
+done
