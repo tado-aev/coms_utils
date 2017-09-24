3d785f40ca81523abc9b534ff659ab255c49e047
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2017-09-24 19:55:02 +0900

    Show unpushed commits

--- scripts/check_dirty.bash
+++ scripts/check_dirty.bash
@@ -27,10 +27,18 @@ for repo in ${valid_repos[@]}; do
     echo "> $( basename $repo )"
     res="$( cd $repo && \
         [[ -z $( git status --short ) ]] && \
-        echo true || echo false)"
+        echo true || echo false )"
+    unpushed_changes="$( cd $repo && \
+        [[ -n $( git status | \
+            grep 'ahead of [^ ]* by [0-9]* commit' ) ]] && \
+        echo true || echo false )"
     if $res; then
-        echo -e "\e[32mClean\e[0m"
+        echo -n -e "\e[32mClean\e[0m"
     else
-        echo -e "\e[31mDirty\e[0m"
+        echo -n -e "\e[31mDirty\e[0m"
     fi
+    if $unpushed_changes; then
+        echo -n -e " \e[31m(Unpushed commits)\e[0m"
+    fi
+    echo
 done
